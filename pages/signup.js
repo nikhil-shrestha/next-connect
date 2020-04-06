2dedd0d7798c532daf6440d050da5ef8ba3ac7cc
illusionist <nikhil.shrestha1995@gmail.com>
2020-04-06 09:17:43 +0545

    basic functionality for our signup page

--- pages/signup.js
+++ pages/signup.js
@@ -1,27 +1,109 @@
 import { makeStyles } from '@material-ui/core/styles';
-// import Typography from "@material-ui/core/Typography";
-// import Avatar from "@material-ui/core/Avatar";
-// import FormControl from "@material-ui/core/FormControl";
-// import Paper from "@material-ui/core/Paper";
-// import Input from "@material-ui/core/Input";
-// import InputLabel from "@material-ui/core/InputLabel";
-// import Button from "@material-ui/core/Button";
-// import Snackbar from "@material-ui/core/Snackbar";
-// import Dialog from "@material-ui/core/Dialog";
-// import DialogActions from "@material-ui/core/DialogActions";
-// import DialogContent from "@material-ui/core/DialogContent";
-// import DialogContentText from "@material-ui/core/DialogContentText";
-// import DialogTitle from "@material-ui/core/DialogTitle";
-// import Slide from "@material-ui/core/Slide";
-// import Gavel from "@material-ui/icons/Gavel";
-// import VerifiedUserTwoTone from "@material-ui/icons/VerifiedUserTwoTone";
+import Typography from '@material-ui/core/Typography';
+import Avatar from '@material-ui/core/Avatar';
+import FormControl from '@material-ui/core/FormControl';
+import Paper from '@material-ui/core/Paper';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import Button from '@material-ui/core/Button';
+import Snackbar from '@material-ui/core/Snackbar';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Slide from '@material-ui/core/Slide';
+import Gavel from '@material-ui/icons/Gavel';
+import VerifiedUserTwoTone from '@material-ui/icons/VerifiedUserTwoTone';
+
+import { signupUser } from '../lib/auth';
 
 const Signup = () => {
+  const classes = useStyles();
+  const [formData, setFormData] = React.useState({
+    name: '',
+    email: '',
+    password: ''
+  });
 
-    return <div>Signup</div>;
-}
+  const { name, email, password } = formData;
 
-const  useStyles = makeStyles(theme => ({
+  const handleChange = event => {
+    const { name, value } = event.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+
+    const user = {
+      name,
+      email,
+      password
+    };
+
+    console.log(user);
+
+    signupUser(user);
+  };
+
+  return (
+    <div className={classes.root}>
+      <Paper className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <Gavel />
+        </Avatar>
+        <Typography variant="h5" component="h1">
+          Sign Up
+        </Typography>
+
+        <form className={classes.form} onSubmit={handleSubmit}>
+          <FormControl margin="normal" required fullWidth>
+            <InputLabel htmlFor="name">Name</InputLabel>
+            <Input
+              name="name"
+              type="text"
+              onChange={handleChange}
+              value={name}
+            />
+          </FormControl>
+          <FormControl margin="normal" required fullWidth>
+            <InputLabel htmlFor="email">Email</InputLabel>
+            <Input
+              name="email"
+              type="email"
+              onChange={handleChange}
+              value={email}
+            />
+          </FormControl>
+          <FormControl margin="normal" required fullWidth>
+            <InputLabel htmlFor="password">Password</InputLabel>
+            <Input
+              name="password"
+              type="password"
+              onChange={handleChange}
+              value={password}
+            />
+          </FormControl>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Sign up
+          </Button>
+        </form>
+      </Paper>
+    </div>
+  );
+};
+
+const useStyles = makeStyles(theme => ({
   root: {
     width: 'auto',
     display: 'block',
