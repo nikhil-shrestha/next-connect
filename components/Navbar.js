7fb8c349072fd60092ea307e6190bca46afd5b0d
illusionist <nikhil.shrestha1995@gmail.com>
2020-04-09 07:37:50 +0545

    added nprogress

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,12 +1,26 @@
+import Router from 'next/router';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import ShareOutlined from '@material-ui/icons/ShareOutlined';
+import NProgress from 'nprogress';
 
 import ActiveLink from './ActiveLink';
 import { signoutUser } from '../lib/auth';
+
+Router.events.on('routeChangeStart', url => {
+  console.log('App is changing to: ', url);
+  NProgress.start();
+});
+Router.events.off('routeChangeStart', () => NProgress.done());
+Router.events.on('routeChangeError', (err, url) => {
+  if (err.cancelled) {
+    console.log(`Route to ${url} was cancelled!`);
+  }
+  NProgress.done();
+});
 
 const Navbar = ({ router, pageProps: { auth } }) => {
   const classes = useStyles();
