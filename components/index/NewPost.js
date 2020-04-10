50b00dfaab15f4162ed1967bb0e84a53810b72fa
illusionist <nikhil.shrestha1995@gmail.com>
2020-04-10 14:35:03 +0545

    updating state with NewPost

--- components/index/NewPost.js
+++ components/index/NewPost.js
@@ -1,24 +1,91 @@
 import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import Typography from '@material-ui/core/Typography';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import AddAPhoto from '@material-ui/icons/AddAPhoto';
 
-const NewPost = () => {
-  return <div>NewPost</div>;
+const NewPost = (props) => {
+  const classes = useStyles();
+
+  const { auth, text, image, handleChange } = props;
+
+  return (
+    <Card className={classes.card}>
+      <CardHeader
+        avatar={<Avatar src={auth.user.avatar} />}
+        title={
+          <Typography variant="h6" component="h2">
+            {auth.user.name}
+          </Typography>
+        }
+        className={classes.cardHeader}
+      />
+      <CardContent className={classes.cardContent}>
+        <TextField
+          label="Add a status"
+          value={text}
+          name="text"
+          multiline
+          row="2"
+          fullWidth
+          margin="normal"
+          onChange={handleChange}
+          variant="outlined"
+          InputLabelProps={{
+            shrink: true,
+          }}
+          placeholder={`What's on your mind, ${auth.user.name}`}
+        />
+        <input
+          accept="image/*"
+          type="file"
+          id="image"
+          name="image"
+          onChange={handleChange}
+          className={classes.input}
+        />
+        <label htmlFor="image" className={classes.uploadButton}>
+          <IconButton color="secondary" component="span">
+            <AddAPhoto />
+          </IconButton>
+        </label>
+        <span>{image && image.name}</span>
+      </CardContent>
+      <CardActions className={classes.cardActions}>
+        <Button
+          color="primary"
+          variant="contained"
+          disabled={!text}
+          className={classes.submit}
+        >
+          Post
+        </Button>
+      </CardActions>
+    </Card>
+  );
 };
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   card: {
     marginBottom: theme.spacing(2 * 3),
-    backgroundColor: theme.palette.primary.light
+    backgroundColor: theme.palette.primary.light,
   },
   cardContent: {
-    backgroundColor: 'white'
+    backgroundColor: 'white',
   },
   input: {
-    display: 'none'
+    display: 'none',
   },
   cardActions: {
     display: 'flex',
-    flexDirection: 'row-reverse'
-  }
+    flexDirection: 'row-reverse',
+  },
 }));
 
 export default NewPost;
--- components/index/PostFeed.js
+++ components/index/PostFeed.js
@@ -1,16 +1,61 @@
+import { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
 
-const PostFeed = () => {
-  return <div>PostFeed</div>;
+import NewPost from './NewPost';
+import Post from './Post';
+
+const PostFeed = (props) => {
+  const classes = useStyles();
+
+  const { auth } = props;
+
+  const [posts, setPosts] = useState([]);
+  const [text, setText] = useState('');
+  const [image, setImage] = useState('');
+
+  const handleChange = (event) => {
+    const { name, value, files } = event.target;
+
+    let inputValue;
+
+    if (name === 'image') {
+      inputValue = files[0];
+      setImage(inputValue);
+    } else {
+      inputValue = value;
+      setText(inputValue);
+    }
+  };
+
+  return (
+    <div className={classes.root}>
+      <Typography
+        variant="h4"
+        component="h1"
+        align="center"
+        color="primary"
+        className={classes.title}
+      >
+        Post Feed
+      </Typography>
+      <NewPost
+        auth={auth}
+        text={text}
+        image={image}
+        handleChange={handleChange}
+      />
+    </div>
+  );
 };
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   root: {
-    paddingBottom: theme.spacing(2) * 2
+    paddingBottom: theme.spacing(2) * 2,
   },
   title: {
-    padding: theme.spacing(2) * 2
-  }
+    padding: theme.spacing(2) * 2,
+  },
 }));
 
 export default PostFeed;
