148ccdbc2c4c9c5342c88ac94663a65897cea1b9
illusionist <nikhil.shrestha1995@gmail.com>
2020-04-10 09:36:57 +0545

    basic splash screen

--- pages/index.js
+++ pages/index.js
@@ -1,52 +1,102 @@
 import { makeStyles } from '@material-ui/core/styles';
-// import CircularProgress from "@material-ui/core/CircularProgress";
-// import Drawer from "@material-ui/core/Drawer";
-// import Typography from "@material-ui/core/Typography";
-// import Grid from "@material-ui/core/Grid";
-// import Button from "@material-ui/core/Button";
+import CircularProgress from '@material-ui/core/CircularProgress';
+import Drawer from '@material-ui/core/Drawer';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+import Router from 'next/router';
 
 import { authInitialProps } from '../lib/auth';
 
-export default function Index() {
-  return <div>Index</div>;
+export default function Index({ auth }) {
+  const classes = useStyles();
+  return (
+    <main className={classes.root}>
+      {auth.user && auth.user._id ? (
+        <div>Auth User Page</div>
+      ) : (
+        <Grid
+          justify="center"
+          alignItems="center"
+          container
+          className={classes.heroContent}
+        >
+          <Typography
+            component="h1"
+            variant="h2"
+            align="center"
+            gutterBottom
+            color="textPrimary"
+          >
+            A Better Social Network
+          </Typography>
+          <Typography
+            variant="h6"
+            align="center"
+            color="textPrimary"
+            component="p"
+          >
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s
+            with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus
+            PageMaker including versions of Lorem Ipsum.
+          </Typography>
+          <Button
+            className={classes.fabButton}
+            variant="contained"
+            color="primary"
+            onClick={() => Router.pus('/signup')}
+          >
+            Get Started
+          </Button>
+        </Grid>
+      )}
+    </main>
+  );
 }
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   root: {
-    paddingTop: theme.spacing(2) * 10,
-    paddingLeft: theme.spacing(2) * 5,
+    paddingTop: theme.spacing(10),
+    paddingLeft: theme.spacing(5),
     [theme.breakpoints.down('sm')]: {
-      paddingRight: theme.spacing(2) * 5
-    }
+      paddingRight: theme.spacing(5),
+    },
   },
   progressContainer: {
-    height: '80vh'
+    height: '80vh',
   },
   progress: {
-    margin: theme.spacing(2) * 2,
-    color: theme.palette.secondary.light
+    margin: theme.spacing(2),
+    color: theme.palette.secondary.light,
   },
   drawerContainer: {
     [theme.breakpoints.down('sm')]: {
-      display: 'none'
-    }
+      display: 'none',
+    },
   },
   drawer: {
-    width: 350
+    width: 350,
   },
   drawerPaper: {
     marginTop: 70,
-    width: 350
+    width: 350,
   },
   fabButton: {
-    margin: theme.spacing(2) * 3
+    borderRadius: 25,
+    margin: theme.spacing(3),
   },
   heroContent: {
     maxWidth: 600,
-    paddingTop: theme.spacing(2) * 8,
-    paddingBottom: theme.spacing(2) * 6,
-    margin: '0 auto'
-  }
+    paddingTop: theme.spacing(8),
+    paddingBottom: theme.spacing(6),
+    margin: '0 auto',
+  },
 }));
 
 Index.getInitialProps = authInitialProps();
